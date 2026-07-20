6581a35c4f88e9f2cecde58592f79ebbbdccd4d2
Zllnn <3247004241@qq.com>
2026-07-20 16:46:28 +0800

    feat：拖拽功能实现

---
+++ src/editor/materials/Page/index.tsx
@@ -0,0 +1,20 @@
+import type { CommonComponentProps } from "../../interface";
+import { useMaterailDrop } from "../../hooks/useMaterialDrop";
+
+function Page({ id, name, children }: CommonComponentProps) {
+
+    //使用useDrop
+    const {canDrop, drop } = useMaterailDrop(['Button', 'Container'], id);
+
+    return (
+        <div
+            ref={drop}
+            className='p-[20px] h-[100%] box-border'
+            style={{ border: canDrop ? '2px solid blue' : 'none' }}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Page;
