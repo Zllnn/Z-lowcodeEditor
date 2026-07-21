a9659b539856af6ec9708db42f6040f888627782
Zllnn <3247004241@qq.com>
2026-07-21 17:36:22 +0800

    feat：预览功能

--- src/editor/stores/component-config.tsx
+++ src/editor/stores/component-config.tsx
@@ -1,91 +1,98 @@
-import { create } from "zustand";
-import type { ComponentType } from "react";
-import Container from "../materials/Container";
-import Button from "../materials/Button";
-import Page from "../materials/Page";
-import type { CommonComponentProps } from "../interface";
-
-export interface ComponentConfig {
-  name: string;
-  defaultProps: Record<string, unknown>;
-  desc: string;
-  setter?: ComponentSetter[];
-  stylesSetter?: ComponentSetter[];
-  component: ComponentType<CommonComponentProps>;
-}
+import {create} from 'zustand';
+import ContainerDev from '../materials/Container/dev';
+import ContainerProd from '../materials/Container/prod';
+import ButtonDev from '../materials/Button/dev';
+import ButtonProd from '../materials/Button/prod';
+import PageDev from '../materials/Page/dev';
+import PageProd from '../materials/Page/prod';
 
 export interface ComponentSetter {
-  name: string;
-  label: string;
-  type: string;
-  [key: string]: any;
+    name: string;
+    label: string;
+    type: string;
+    [key: string]: any;
+}
+
+export interface ComponentConfig {
+    name: string;
+    defaultProps: Record<string, any>,
+    desc: string;
+    setter?: ComponentSetter[],
+    stylesSetter?: ComponentSetter[]
+    dev: any;
+    prod: any;
 }
 
 interface State {
-  componentConfig: { [key: string]: ComponentConfig };
+    componentConfig: {[key: string]: ComponentConfig};
 }
 
 interface Action {
-  registerComponent: (name: string, componentConfig: ComponentConfig) => void;
+    registerComponent: (name: string, componentConfig: ComponentConfig) => void
 }
 
 export const useComponentConfigStore = create<State & Action>((set) => ({
-  componentConfig: {
-    Container: {
-      name: "Container",
-      defaultProps: {},
-      component: Container,
+    componentConfig: {
+        Container: {
+            name: 'Container',
+            defaultProps: {},
+            desc: '容器',
+            dev: ContainerDev,
+            prod: ContainerProd
+        },
+        Button: {
+            name: 'Button',
+            defaultProps: {
+                type: 'primary',
+                text: '按钮'
+            },
+            setter: [
+                {
+                  name: 'type',
+                  label: '按钮类型',
+                  type: 'select',
+                  options: [
+                    {label: '主按钮', value: 'primary'},
+                    {label: '次按钮', value: 'default'},
+                  ],
+                },
+                {
+                  name: 'text',
+                  label: '文本',
+                  type: 'input',
+                },
+            ],
+            stylesSetter: [
+                {
+                    name: 'width',
+                    label: '宽度',
+                    type: 'inputNumber',
+                },
+                {
+                    name: 'height',
+                    label: '高度',
+                    type: 'inputNumber',
+                }
+            ],
+            desc: '按钮',
+            dev: ButtonDev,
+            prod: ButtonProd
+        },
+        Page: {
+            name: 'Page',
+            defaultProps: {},
+            desc: '页面',
+            dev: PageDev,
+            prod: PageProd
+        }
     },
-    Button: {
-      name: "Button",
-      defaultProps: {
-        type: "primary",
-        text: "按钮",
-      },
-      setter: [
-        {
-          name: "type",
-          label: "按钮类型",
-          type: "select",
-          options: [
-            { label: "主按钮", value: "primary" },
-            { label: "次按钮", value: "default" },
-          ],
-        },
-        {
-          name: "text",
-          label: "文本",
-          type: "input",
-        },
-      ],
-      stylesSetter: [
-        {
-          name: "width",
-          label: "宽度",
-          type: "inputNumber",
-        },
-        {
-          name: "height",
-          label: "高度",
-          type: "inputNumber",
-        },
-      ],
-      component: Button,
-    },
-    Page: {
-      name: "Page",
-      defaultProps: {},
-      component: Page,
-    },
-  },
-  registerComponent: (name, componentConfig) =>
-    set((state) => {
-      return {
-        ...state,
-        componentConfig: {
-          ...state.componentConfig,
-          [name]: componentConfig,
-        },
-      };
-    }),
+    registerComponent: (name, componentConfig) => set((state) => {
+        return {
+            ...state,
+            componentConfig: {
+                ...state.componentConfig,
+                [name]: componentConfig
+            }
+        }
+    })
 }));
