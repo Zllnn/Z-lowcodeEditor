4e5898d78b6184494f52493d504b605ad39a1c05
Zllnn <3247004241@qq.com>
2026-07-21 15:44:39 +0800

    feat： 自定义css设置

--- src/editor/components/Setting/ComponentStyle.tsx
+++ src/editor/components/Setting/ComponentStyle.tsx
@@ -1,39 +1,66 @@
-import { Form, Input, InputNumber, Select } from 'antd';
-import { CSSProperties, useEffect } from 'react';
-import { ComponentConfig, ComponentSetter, useComponentConfigStore } from '../../stores/component-config';
-import { useComponetsStore } from '../../stores/components';
+import { Form, Input, InputNumber, Select } from "antd";
+import { CSSProperties, useEffect } from "react";
+import {
+  ComponentConfig,
+  ComponentSetter,
+  useComponentConfigStore,
+} from "../../stores/component-config";
+import { useComponetsStore } from "../../stores/components";
+import { CssEditor } from "../CssEditor";
+import { debounce } from "lodash-es";
 
 export function ComponentStyle() {
-
   const [form] = Form.useForm();
 
-  const { curComponentId, curComponent, updateComponentStyles } = useComponetsStore();
+  const { curComponentId, curComponent, updateComponentStyles } =
+    useComponetsStore();
   const { componentConfig } = useComponentConfigStore();
 
   useEffect(() => {
     const data = form.getFieldsValue();
-    form.setFieldsValue({...data, ...curComponent?.styles});
-  }, [curComponent])
+    form.setFieldsValue({ ...data, ...curComponent?.styles });
+  }, [curComponent]);
 
   if (!curComponentId || !curComponent) return null;
 
   function renderFormElememt(setting: ComponentSetter) {
     const { type, options } = setting;
-  
-    if (type === 'select') {
-      return <Select options={options} />
-    } else if (type === 'input') {
-      return <Input />
-    } else if (type === 'inputNumber') {
-        return <InputNumber />
+
+    if (type === "select") {
+      return <Select options={options} />;
+    } else if (type === "input") {
+      return <Input />;
+    } else if (type === "inputNumber") {
+      return <InputNumber />;
     }
   }
 
   function valueChange(changeValues: CSSProperties) {
     if (curComponentId) {
-        updateComponentStyles(curComponentId, changeValues);
+      updateComponentStyles(curComponentId, changeValues);
     }
   }
+  const handleEditorChange = debounce((value) => {
+    setCss(value);
+
+    let css: Record<string, any> = {};
+
+    try {
+      const cssStr = value
+        .replace(/\/\*.*\*\//, "") // 去掉注释 /** */
+        .replace(/(\.?[^{]+{)/, "") // 去掉 .comp {
+        .replace("}", ""); // 去掉 }
+
+      styleToObject(cssStr, (name, value) => {
+        css[
+          name.replace(/-\w/, (item) => item.toUpperCase().replace("-", ""))
+        ] = value;
+      });
+
+      console.log(css);
+      updateComponentStyles(curComponentId, css);
+    } catch (e) {}
+  }, 500);
 
   return (
     <Form
@@ -42,13 +69,14 @@ export function ComponentStyle() {
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 14 }}
     >
-      {
-        componentConfig[curComponent.name]?.stylesSetter?.map(setter => (
-          <Form.Item key={setter.name} name={setter.name} label={setter.label}>
-            {renderFormElememt(setter)}
-          </Form.Item>
-        ))
-      }
+      {componentConfig[curComponent.name]?.stylesSetter?.map((setter) => (
+        <Form.Item key={setter.name} name={setter.name} label={setter.label}>
+          {renderFormElememt(setter)}
+        </Form.Item>
+      ))}
+      <div className="h-[200px] border-[1px] border-[#ccc]">
+        <CssEditor value={`.comp{\n\n}`} onCange={handleEditorChange} />
+      </div>
     </Form>
-  )
+  );
 }
