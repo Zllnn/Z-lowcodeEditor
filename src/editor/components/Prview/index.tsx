eb4bf16d02adcefb91006ef5401ff4a49abd48e5
Zllnn <3247004241@qq.com>
2026-07-22 12:38:48 +0800

    feat: 事件绑定操作

--- src/editor/components/Prview/index.tsx
+++ src/editor/components/Prview/index.tsx
@@ -1,35 +1,59 @@
 import React from "react";
 import { useComponentConfigStore } from "../../stores/component-config";
-import { Component, useComponetsStore } from "../../stores/components"
+import { Component, useComponetsStore } from "../../stores/components";
 
 export function Preview() {
-    const { components } = useComponetsStore();
-    const { componentConfig } = useComponentConfigStore();
+  const { components } = useComponetsStore();
+  const { componentConfig } = useComponentConfigStore();
 
-    function renderComponents(components: Component[]): React.ReactNode {
-        return components.map((component: Component) => {
-            const config = componentConfig?.[component.name]
+  function handleEvent(component: Component) {
+    const props: Record<string, any> = {};
 
-            if (!config?.prod) {
-                return null;
+    componentConfig[component.name].events?.forEach((event) => {
+      const eventConfig = component.props[event.name];
+
+      if (eventConfig) {
+        const { type } = eventConfig;
+
+        props[event.name] = () => {
+          if (type === "goToLink" && eventConfig.url) {
+            window.location.href = eventConfig.url;
+          } else if (type === "showMessage" && eventConfig.config) {
+            if (eventConfig.config.type === "success") {
+              message.success(eventConfig.config.text);
+            } else if (eventConfig.config.type === "error") {
+              message.error(eventConfig.config.text);
             }
-            
-            return React.createElement(
-                config.prod,
-                {
-                    key: component.id,
-                    id: component.id,
-                    name: component.name,
-                    styles: component.styles,
-                    ...config.defaultProps,
-                    ...component.props,
-                },
-                renderComponents(component.children || [])
-            )
-        })
-    }
+          }
+        };
+      }
+    });
+    return props;
+  }
 
-    return <div>
-        {renderComponents(components)}
-    </div>
+  function renderComponents(components: Component[]): React.ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config?.prod) {
+        return null;
+      }
+
+      return React.createElement(
+        config.prod,
+        {
+          key: component.id,
+          id: component.id,
+          name: component.name,
+          styles: component.styles,
+          ...config.defaultProps,
+          ...component.props,
+          ...handleEvent(component),
+        },
+        renderComponents(component.children || []),
+      );
+    });
+  }
+
+  return <div>{renderComponents(components)}</div>;
 }
--- src/editor/components/Setting/ComponentEvent.tsx
+++ src/editor/components/Setting/ComponentEvent.tsx
@@ -1,3 +1,79 @@
+import { Collapse, Input, Select, CollapseProps } from "antd";
+import { useComponetsStore } from "../../stores/components";
+import { useComponentConfigStore } from "../../stores/component-config";
+
 export function ComponentEvent() {
-    return <div>ComponentEvent</div>
+  const { curComponentId, curComponent, updateComponentProps } =
+    useComponetsStore();
+  const { componentConfig } = useComponentConfigStore();
+
+  if (!curComponent) return null;
+
+  function selectAction(eventName: string, value: string) {
+    if (!curComponentId) return;
+
+    updateComponentProps(curComponentId, { [eventName]: { type: value } });
+  }
+
+  function urlChange(eventName: string, value: string) {
+    if (!curComponentId) return;
+
+    updateComponentProps(curComponentId, {
+      [eventName]: {
+        ...curComponent?.props?.[eventName],
+        url: value,
+      },
+    });
+  }
+
+  const items: CollapseProps["items"] = (
+    componentConfig[curComponent.name].events || []
+  ).map((event) => {
+    return {
+      key: event.name,
+      label: event.label,
+      children: (
+        <div>
+          <div className="flex items-center">
+            <div>动作：</div>
+            <Select
+              className="w-[160px]"
+              options={[
+                { label: "显示提示", value: "showMessage" },
+                { label: "跳转链接", value: "goToLink" },
+              ]}
+              onChange={(value) => {
+                selectAction(event.name, value);
+              }}
+              value={curComponent?.props?.[event.name]?.type}
+            />
+          </div>
+          {curComponent?.props?.[event.name]?.type === "goToLink" && (
+            <div className="mt-[10px]">
+              <div className="flex items-center gap-[10px]">
+                <div>链接</div>
+                <div>
+                  <Input
+                    onChange={(e) => {
+                      urlChange(event.name, e.target.value);
+                    }}
+                    value={curComponent?.props?.[event.name]?.url}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          {curComponent?.props?.[event.name]?.type === "showMessage" && (
+            <ShowMessage event={event} />
+          )}
+        </div>
+      ),
+    };
+  });
+
+  return (
+    <div className="px-[10px]">
+      <Collapse className="mb-[10px]" items={items} />
+    </div>
+  );
 }
