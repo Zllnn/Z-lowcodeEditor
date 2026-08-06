fdc208d881cb8152b2d5aeb7ddfe190baf4df65a
Zllnn <3247004241@qq.com>
2026-08-06 18:04:28 +0800

    fix：支持编辑态按钮拖拽移动

--- src/editor/materials/Button/dev.tsx
+++ src/editor/materials/Button/dev.tsx
@@ -1,6 +1,7 @@
 import { Button as AntdButton } from 'antd';
 import type { ButtonType } from 'antd/es/button';
 import type { ReactNode } from 'react';
+import { useDrag } from 'react-dnd';
 import type { CommonComponentProps } from '../../interface';
 
 interface ButtonProps extends CommonComponentProps {
@@ -9,8 +10,16 @@ interface ButtonProps extends CommonComponentProps {
 }
 
 const Button = ({id, type, text, styles}: ButtonProps) => {
+  const [, connectDrag] = useDrag(() => ({
+    type: 'Button',
+    item: { type: 'Button', dragType: 'move' as const, id },
+  }), [id]);
+  const drag = (node: HTMLAnchorElement | HTMLButtonElement | null) => {
+    connectDrag(node);
+  };
+
   return (
-    <AntdButton data-component-id={id} type={type} style={styles}>{text}</AntdButton>
+    <AntdButton ref={drag} data-component-id={id} type={type} style={styles}>{text}</AntdButton>
   )
 }
 
